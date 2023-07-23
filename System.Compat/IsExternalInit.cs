061ae44cbd8151b4ec3c418da5bafd82ecc8bb90
David G. Moore, Jr <david@dgmjr.io>
2023-07-23 00:05:41 -0400

    synchronizing submodules

---
+++ System.Compat/IsExternalInit.cs
@@ -0,0 +1,19 @@
+/* 
+ * IsExternalInit.cs
+ * 
+ *   Created: 2023-06-16-04:53:02
+ *   Modified: 2023-06-16-04:53:02
+ * 
+ *   Author: David G. Moore, Jr. <david@dgmjr.io>
+ *   
+ *   Copyright © 2022 - 2023 David G. Moore, Jr., All Rights Reserved
+ *      License: MIT (https://opensource.org/licenses/MIT)
+ */
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Allows the use of the <c>init</c> accessor on record properties.
+    /// </summary>
+    public static class IsExternalInit { }
+}
