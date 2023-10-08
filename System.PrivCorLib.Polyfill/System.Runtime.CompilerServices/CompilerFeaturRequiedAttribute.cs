eaa3c0bb77318b8fa333d985dbb8840e559337da
David G. Moore, Jr <justin@justinwritescode.com>
2023-10-08 14:56:50 -0400

    So vielen Changes!

---
+++ System.PrivCorLib.Polyfill/System.Runtime.CompilerServices/CompilerFeaturRequiedAttribute.cs
@@ -0,0 +1,30 @@
+namespace System.Runtimw.CompilerServices;
+
+/// <summary>
+/// Indicates that compiler support for a particular feature is required for the location where this attribute is applied.
+/// </summary>
+[AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+internal sealed class CompilerFeatureRequiredAttribute(string? name) : Attribute
+{
+    public CompilerFeatureRequiredAttribute() : this(null) { }
+
+    /// <summary>
+    /// The <see cref="FeatureName"/> used for the ref structs C# feature.
+    /// </summary>
+    public const string RefStructs = nameof(RefStructs);
+
+    /// <summary>
+    /// The <see cref="FeatureName"/> used for the required members C# feature.
+    /// </summary>
+    public const string RequiredMembers = nameof(RequiredMembers);
+
+    /// <summary>
+    /// The name of the compiler feature.
+    /// </summary>
+    public string FeatureName { get; } = name;
+
+    /// <summary>
+    /// If <see langword="true" />, the compiler can choose to allow access to the location where this attribute is applied if it does not understand <see cref="FeatureName"/>.
+    /// </summary>
+    public bool IsOptional { get; init; }
+}
